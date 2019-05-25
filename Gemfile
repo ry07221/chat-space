02ba9727c4c2c2a4eeff845769441c3716e02eae
ry <zijoyrb@gamil.com>
2019-05-25 15:31:16 +0900

    メッセージ送信機能

--- Gemfile
+++ Gemfile
@@ -56,4 +56,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
   gem 'erb2haml'
   gem 'devise'
   gem 'font-awesome-rails'
+  gem 'carrierwave'
+  gem 'mini_magick'
+  
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root 'groups#index'
-  resource :user, only: [:edit, :update]
+  
+  resource :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
+
